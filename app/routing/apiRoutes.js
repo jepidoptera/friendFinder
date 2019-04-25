247a34f0fc89c4c0770718f3403a5077993b568a
jepidoptera <jepidoptera@live.com>
2019-04-24 21:03:46 -0500

    survey basic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ module.exports = function apiRoutes() {
     var authToken = "";
     friends.init();
     
-    app.post("/login", function(req, res) {
+    app.post("/api/login", function(req, res) {
         var password= req.body.password;
         console.log("login attempt");
         console.log('password = ' + password);
@@ -20,15 +20,17 @@ module.exports = function apiRoutes() {
         }        
     });
     
-    app.get("/survey-basic", function (req, res) {
-        
-    });
-
-    app.get("/friends", function(req, res) {
+    app.get("/api/friends", function(req, res) {
         res.send("everyone is your friend.");
     });
 
-    app.post("/register", function(req, res) {
+    app.post("/api/answer", function(req, res) {
+        // received an answer to a survey question
+        console.log('answered: ' + req.body.answer);
+        res.send('ok');
+    });
+
+    app.post("/api/register", function(req, res) {
         console.log("registering new user...");
         var success = friends.newUser(req.body);
         var message = "";
@@ -47,4 +49,7 @@ module.exports = function apiRoutes() {
         }
     });
 
+    app.post("/api/match", function (req, res) {
+        res.send("You don't match with anyone.  You will always be lonely.");
+    });
 };
